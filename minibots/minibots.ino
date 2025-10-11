b1cd1ab5ec8a466e29f21dd054fc2c3c1da8f929
Srikar Andra <67433991+s-andra21@users.noreply.github.com>
2025-10-11 11:16:01 -0700

    wrote sample minibots code

--- minibots/minibots.ino
+++ minibots/minibots.ino
@@ -2,10 +2,10 @@
 
 // Create minibot object with robot ID and motor pins
 // Preferred motor pins: left=16, right=17, dcMotor=18, servoMotor=19
-Minibot bot("YOUR NAME HERE");
+Minibot bot("Sample Robot");
 
 void setup() {
-
+  bot.begin();
 }
 
 void loop() {
@@ -13,5 +13,22 @@ void loop() {
   bot.updateController();
 
   // TODO: Write your robot control code here
+  if (bot.GetGameStatus() == "teleop"){
+    //This is Tank Drive which means each joystick controls that side's respective motor
+    float leftSpeed = (bot.getLeftY() - 127.5) / 127.5;//Speed is proportional to how far you move joystick.
+    float rightSpeed = (bot.getRightY() - 127.5) / 127.5;
+    bot.driveLeftMotor(leftSpeed);
+    bot.driveRightMotor(rightSpeed);
 
+    //Servo motors for manipulator use
+    if (bot.getSquare()) {
+      bot.driveServoMotor(-30);//Turn motor left
+    } 
+    else if (bot.getTriangle()) {
+      bot.driveServoMotor(30);//right
+    }
+    else {
+      bot.driveServoMotor(0);//center
+    }
+  } 
 }
